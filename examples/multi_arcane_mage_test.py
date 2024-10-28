053538237cc296a233ee6d2e626429fbfb0d093b
Pepopo <pepopo978@gmail.com>
2024-10-27 21:22:20 -0700

    fix cooldowns not defined

--- examples/multi_arcane_mage_test.py
+++ examples/multi_arcane_mage_test.py
@@ -4,7 +4,7 @@ mages = []
 num_mages = 3
 
 for i in range(num_mages):
-    if i==0:
+    if i == 0:
         fm = Mage(name=f'procs', sp=1000, crit=40, hit=15,
                   tal=ArcaneMageTalents,
                   opts=MageOptions(),
@@ -12,15 +12,20 @@ for i in range(num_mages):
                       ornate_bloodstone_dagger=True,
                       wrath_of_cenarius=True,
                   ))
+        fm.arcane_surge_rupture_missiles()
     else:
         fm = Mage(name=f'reg{i}', sp=1000, crit=40, hit=15,
                   tal=ArcaneMageTalents,
-                  opts=MageOptions())
+                  opts=MageOptions(),
+                  equipped_items=EquippedItems(
+                      ornate_bloodstone_dagger=True,
+                      wrath_of_cenarius=True,
+                  ))
 
-    fm.arcane_surge_rupture_missiles(cds=CooldownUsages(arcane_power=0))
+        fm.arcane_surge_rupture_missiles(cds=CooldownUsages(arcane_power=[0, 180]))
     # fm.arcane_rupture_surge_missiles(cds=CooldownUsages(arcane_power=0))
     mages.append(fm)
 
 sim = Simulation(characters=mages)
-sim.run(iterations=1000, duration=180)
+sim.run(iterations=1000, duration=250)
 sim.detailed_report()
